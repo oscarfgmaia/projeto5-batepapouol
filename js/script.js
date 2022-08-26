4c6eed52f892b6a5bdbfd186182fb38a122ea0bc
OscarMaia <oscarfgmaia@gmail.com>
2022-08-25 22:00:11 -0300

    get and post working also the participants

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
 let messages;
+let username = "Oscar";
+let deliveryTo;
+let textMessage;
+let typeMessage;
 
-const getPromise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
-getPromise.then(processPromise)
+const user = {
+    name: username
+}
 
+
+function getParticipants() {
+    const getParticipantsPromise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
+    getParticipantsPromise.then(verifyParticipants)
+}
+function verifyParticipants(promise) {
+    const participants = promise.data
+    const usersContainer = document.querySelector('.users-container');
+    console.log(participants)
+    console.log(participants[6].name)
+    for (let i = 0; i < participants.length; i++) {
+        usersContainer.innerHTML += `
+        <div class="user-container">
+            <div class="icon-name">
+                <div class="users"><ion-icon name="person-circle-sharp"></ion-icon></div>
+                <span class="send-to">${participants[i].name}</span>
+            </div>
+            <span class="check hidden"><ion-icon name="checkmark-sharp"></ion-icon></span>
+        </div>
+        `
+    }
+}
+function openMenu(){
+    const element = document.querySelector('.menu')
+    element.classList.remove('hidden')
+    getParticipants()
+}
 function processPromise(promise) {
     const messageContainer = document.querySelector(".message-container")
     messages = promise.data
@@ -42,6 +74,63 @@ function processPromise(promise) {
             `
         }
     }
-    messageContainer.children[messages.length - 1].scrollIntoView()
+    messageContainer.lastElementChild.scrollIntoView()
 }
 
+
+
+function logIn() {
+    const postPromiseLogIn = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', user)
+    postPromiseLogIn.then(updateChat)
+    postPromiseLogIn.catch(userFailedToLogIn)
+    setInterval(userStayOn, 5000)
+    //setInterval(updateChat,1000)
+}
+function userStayOn() {
+    const userOnline = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', user);
+    console.log("User stay on :)")
+}
+
+function updateChat() {
+    const getPromise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    getPromise.then(processPromise)
+}
+
+
+function userFailedToLogIn(erro) {
+    console.log("Login Failed: " + erro.response.status)
+    switch (erro.response.status) {
+        case 400:
+            alert("Usuário já se encontra logado")
+            break;
+        default:
+            alert(erro.response.status)
+            break;
+    }
+}
+
+let messageTyped = document.querySelector('footer input');
+
+messageTyped.addEventListener("keypress", function (event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        sendMessage()
+    }
+});
+
+function sendMessage() {
+    let messageTyped = document.querySelector('footer input');
+    const messageBody = {
+        from: username,
+        to: "Todos",
+        text: messageTyped.value,
+        type: "message" // ou "private_message" para o bônus
+    }
+
+    const postPromiseDeliveryMessage = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', messageBody)
+    postPromiseDeliveryMessage.catch(messageFailedToDelivery)
+    messageTyped.value = ''
+}
+function messageFailedToDelivery(erro) {
+    console.log("Delivery Message: " + erro.response.status)
+}
