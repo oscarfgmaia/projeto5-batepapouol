034d0a933a656ce2c46a23b2595751c113c9c27b
OscarMaia <oscarfgmaia@gmail.com>
2022-08-27 23:44:28 -0300

    private message and choose participant working

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 let messages;
-let username = "Oscar";
-let deliveryTo;
-let textMessage;
-let typeMessage;
-
+let deliveryTo ='Todos';
+let typeMessage = "message" //ou "private_message" para o bônus
 const user = {
-    name: username
+    name: ''
+}
+function setPublicMessage(){
+    typeMessage = 'message'
+    getParticipants()
+}
+function setPrivateMessage(){
+    typeMessage = 'private_message'
+    getParticipants()
 }
 
 
+
+function sendUserName() {
+    const input = document.getElementById('usernameId');
+    const button = document.getElementById('button');
+    const spinner = document.querySelector('.spinner')
+    user.name = input.value
+    input.classList.add('hidden')
+    button.classList.add('hidden')
+    spinner.classList.remove('hidden')
+    logIn()
+}
 function getParticipants() {
     const getParticipantsPromise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
     getParticipantsPromise.then(verifyParticipants)
@@ -16,21 +32,62 @@ function getParticipants() {
 function verifyParticipants(promise) {
     const participants = promise.data
     const usersContainer = document.querySelector('.users-container');
-    console.log(participants)
-    console.log(participants[6].name)
+    if(typeMessage === "message"){
+        let publicMessage = document.querySelector('.public');
+        let privateMessage = document.querySelector('.private');
+        publicMessage.classList.remove('hidden')
+        privateMessage.classList.add('hidden')        
+    }
+    if(typeMessage === "private_message"){
+        let publicMessage = document.querySelector('.public');
+        let privateMessage = document.querySelector('.private');
+        privateMessage.classList.remove('hidden')
+        publicMessage.classList.add('hidden')
+    }
+    usersContainer.innerHTML =
+        `
+    <div class="user-container selected" onclick="selectUser(this)">
+        <div class="icon-name">
+            <div class="users"><ion-icon name="people"></ion-icon></div>
+            <span class="send-to">Todos</span>
+        </div>
+        <span class="check"><ion-icon name="checkmark-sharp"></ion-icon></span>
+    </div>
+    `;
     for (let i = 0; i < participants.length; i++) {
-        usersContainer.innerHTML += `
-        <div class="user-container">
+        if (participants[i].name === deliveryTo){
+            const todosContainer = document.querySelector('.user-container') 
+            todosContainer.querySelector('.check').classList.add('hidden')
+            todosContainer.classList.remove('selected')
+            usersContainer.innerHTML += `
+            <div class="user-container selected" onclick="selectUser(this)">
             <div class="icon-name">
-                <div class="users"><ion-icon name="person-circle-sharp"></ion-icon></div>
-                <span class="send-to">${participants[i].name}</span>
+            <div class="users"><ion-icon name="person-circle-sharp"></ion-icon></div>
+            <span class="send-to">${participants[i].name}</span>
+            </div>
+            <span class="check"><ion-icon name="checkmark-sharp"></ion-icon></span>
+            </div>
+            `
+        }
+        else{
+            usersContainer.innerHTML += `
+            <div class="user-container" onclick="selectUser(this)">
+            <div class="icon-name">
+            <div class="users"><ion-icon name="person-circle-sharp"></ion-icon></div>
+            <span class="send-to">${participants[i].name}</span>
             </div>
             <span class="check hidden"><ion-icon name="checkmark-sharp"></ion-icon></span>
-        </div>
-        `
+            </div>
+            `
+        }
     }
 }
-function openMenu(){
+function selectUser(user){
+    user.classList.add('selected')
+    deliveryTo = user.querySelector('.send-to').innerHTML
+    getParticipants()
+}
+function openMenu() {
     const element = document.querySelector('.menu')
     element.classList.remove('hidden')
     getParticipants()
@@ -43,33 +100,33 @@ function processPromise(promise) {
         if (messages[i].type === "status") {
             messageContainer.innerHTML += `
             <div class="message-content status">
-                <span class="time">(${messages[i].time})&nbsp</span>
-                <span class="message-from">${messages[i].from}&nbsp</span>
-                <span class="message-text">${messages[i].text}&nbsp</span>
+                <span class="time">(${messages[i].time})</span>
+                <span class="message-from">${messages[i].from}</span>
+                <span class="message-text">${messages[i].text}</span>
             </div>
             `
         }
         if (messages[i].type === "message") {
             messageContainer.innerHTML += `
             <div class="message-content message">
-                <span class="time">(${messages[i].time})&nbsp</span>
-                <span class="message-from">${messages[i].from}&nbsp</span>
-                <span>para&nbsp</span>
+                <span class="time">(${messages[i].time})</span>
+                <span class="message-from">${messages[i].from}</span>
+                <span>para</span>
                 <span class="message-to">${messages[i].to}</span>
-                <span>:&nbsp</span>
-                <span class="message-text">${messages[i].text}&nbsp</span>
+                <span>:</span>
+                <span class="message-text">${messages[i].text}</span>
             </div>
             `
         }
         if (messages[i].type === "private_message") {
             messageContainer.innerHTML += `
             <div class="message-content private-message">
-                <span class="time">(${messages[i].time})&nbsp</span>
-                <span class="message-from">${messages[i].from}&nbsp</span>
-                <span>para&nbsp</span>
+                <span class="time">(${messages[i].time})</span>
+                <span class="message-from">${messages[i].from}</span>
+                <span>para</span>
                 <span class="message-to">${messages[i].to}</span>
-                <span>:&nbsp</span>
-                <span class="message-text">${messages[i].text}&nbsp</span>
+                <span>:</span>
+                <span class="message-text">${messages[i].text}</span>
             </div>
             `
         }
@@ -84,14 +141,16 @@ function logIn() {
     postPromiseLogIn.then(updateChat)
     postPromiseLogIn.catch(userFailedToLogIn)
     setInterval(userStayOn, 5000)
-    //setInterval(updateChat,1000)
+    setInterval(getParticipants, 10000)
+    setInterval(updateChat, 3000)
 }
 function userStayOn() {
     const userOnline = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', user);
-    console.log("User stay on :)")
 }
 
 function updateChat() {
+    const loginScreen = document.querySelector('.login');
+    loginScreen.classList.add('hidden');
     const getPromise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
     getPromise.then(processPromise)
 }
@@ -102,14 +161,16 @@ function userFailedToLogIn(erro) {
     switch (erro.response.status) {
         case 400:
             alert("Usuário já se encontra logado")
+            window.location.reload()
             break;
         default:
-            alert(erro.response.status)
+            alert("outro erro: " + erro.response.status)
+            window.location.reload()
             break;
     }
 }
 
-let messageTyped = document.querySelector('footer input');
+const messageTyped = document.querySelector('footer input');
 
 messageTyped.addEventListener("keypress", function (event) {
     // If the user presses the "Enter" key on the keyboard
@@ -118,19 +179,37 @@ messageTyped.addEventListener("keypress", function (event) {
     }
 });
 
+const userInput = document.getElementById('usernameId')
+userInput.addEventListener("keypress", function (event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        sendUserName()
+    }
+});
+
+function closeMenu(){
+    const menu = document.querySelector(".menu")
+    menu.classList.add('hidden')
+}
+
 function sendMessage() {
     let messageTyped = document.querySelector('footer input');
-    const messageBody = {
-        from: username,
-        to: "Todos",
-        text: messageTyped.value,
-        type: "message" // ou "private_message" para o bônus
-    }
+    if(messageTyped.value != ''){
 
-    const postPromiseDeliveryMessage = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', messageBody)
-    postPromiseDeliveryMessage.catch(messageFailedToDelivery)
-    messageTyped.value = ''
+        const messageBody = {
+            from: user.name,
+            to: deliveryTo,
+            text: messageTyped.value,
+            type: typeMessage // ou "private_message" para o bônus
+        }
+        
+        const postPromiseDeliveryMessage = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', messageBody)
+        postPromiseDeliveryMessage.then(updateChat)
+        postPromiseDeliveryMessage.catch(messageFailedToDelivery)
+        messageTyped.value = ''
+    }
 }
 function messageFailedToDelivery(erro) {
-    console.log("Delivery Message: " + erro.response.status)
+    alert("User desconected")
+    window.location.reload()
 }
